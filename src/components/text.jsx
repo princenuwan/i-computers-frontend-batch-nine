309db3ab39bbb4495f074e3321dc8a070363a4c4
princenuwan <nuwanuk3@gmail.com>
2026-02-07 19:52:39 +0530

    starting to create admin dash board

---
+++ src/components/text.jsx
@@ -0,0 +1,13 @@
+export default function Test() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-[600px] h-[600px] bg-red-500 flex justify-evenly">
+        <div className="w-[50px] h-[50px] bg-red-100 flex items-center justify-center"> 1 </div>
+        <div className="w-[50px] h-[50px] bg-green-800 flex items-center justify-center"> 1 </div>
+        <div className="w-[50px] h-[50px] bg-blue-400 flex items-center justify-center"> 1 </div>
+        <div className="w-[50px] h-[50px] bg-yellow-300 flex items-center justify-center"> 1 </div>
+        <div className="w-[50px] h-[50px] bg-purple-300 flex items-center justify-center"> 1 </div>
+      </div>
+    </div>
+  );
+}
